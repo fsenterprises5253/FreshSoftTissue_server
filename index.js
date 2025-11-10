50b61cf3a9d5a7d45257f445994224b105d253d7
Hussain <hussain.f.rang29@gmail.com>
2025-11-10 22:49:35 +0530

    move server to src/server 1

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
+// ✅ Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
